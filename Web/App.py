5bcb37351605a62144de61b48743c0bef22a62e3
BogdanTudoseGit <tudosebogdan1291@gmail.com>
2024-11-19 00:05:59 +0200

    Update Created the website, added charts for temperature and humidity

--- Web/App.py
+++ Web/App.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+current_temperature = 58
+current_humidity = 65
 
+@app.route('/')
+def display_temperature():
+    return render_template('index.html', temperature=current_temperature, humidity=current_humidity)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
