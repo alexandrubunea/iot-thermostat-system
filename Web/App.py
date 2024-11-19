2f51613b8cc639d3007ec3b694cdde8358fb5858
BogdanTudoseGit <tudosebogdan1291@gmail.com>
2024-11-19 22:15:15 +0200

    Update Added running time display Added thermostat buttons All the variables now update in real time corresponding to python script variables

--- Web/App.py
+++ Web/App.py
@@ -1,14 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
-
+# Existing variables
 current_temperature = 58
 current_humidity = 65
+running_time = 125
 
 @app.route('/')
 def display_temperature():
-    return render_template('index.html', temperature=current_temperature, humidity=current_humidity)
+    return render_template('index.html')
+
+@app.route('/data')
+def get_data():
+    return jsonify({
+        "temperature": current_temperature,
+        "humidity": current_humidity,
+        "running_time": running_time
+    })
+
+@app.route('/button-press', methods=['POST'])
+def handle_button_press():
+    data = request.get_json()
+    action = data.get('action')
+    print(f"Button pressed: {action}")
+    return jsonify({"status": "success", "action_received": action})
 
 if __name__ == '__main__':
     app.run(debug=True)
