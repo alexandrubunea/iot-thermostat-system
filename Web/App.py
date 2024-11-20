8bb759270d8dbdddbed165d5256cdcc28dfa1efd
Bunea Alexandru <alexandrubunea03@gmail.com>
2024-11-20 21:44:25 +0200

    Update Now the server communicate with the ESP32 on a different thread.

--- Web/App.py
+++ Web/App.py
@@ -1,5 +1,8 @@
+import time
 import requests
 from flask import Flask, render_template, jsonify, request
+import threading
+from threading import Lock
 
 ESP32_IP_ADDRESS = 'http://192.168.1.140/api/'
 
@@ -12,39 +15,58 @@ current_data = {
     'runningTime': None
 }
 
+data_lock = Lock()
+
 def fetch_esp32_data():
     global current_data
+    try:
+        response = requests.get(f'{ESP32_IP_ADDRESS}fetch_data', headers={'Content-Type': 'application/json'})
+        response.raise_for_status()  # Raise an exception for 4xx/5xx responses
+        data = response.json()
 
-    response = requests.get(
-        f'{ESP32_IP_ADDRESS}fetch_data',
-        headers={'Content-Type': 'application/json'}
-    )
-    data = response.json()
+        with data_lock:
+            current_data.update({
+                'temperature': data['currentTemperature'],
+                'humidity': data['currentHumidity'],
+                'target_temperature': data['targetTemperature'],
+                'running_time': data['runningTime']
+            })
 
-    current_data = {
-        'temperature': data['currentTemperature'],
-        'humidity': data['currentHumidity'],
-        'target_temperature': data['targetTemperature'],
-        'running_time': data['runningTime']
-    }
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data from ESP32: {e}")
+    except ValueError as e:
+        print(f"Error parsing response JSON: {e}")
 
 @app.route('/')
 def display_temperature():
-    fetch_esp32_data()
-    return render_template('index.html', **current_data)
+    with data_lock:
+        data = current_data
+    return render_template('index.html', **data)
 
 @app.route('/data')
 def get_data():
-    return jsonify(current_data)
+    with data_lock:
+        data = current_data
+    return jsonify(data)
 
 @app.route('/button-press', methods=['POST'])
 def handle_button_press():
     data = request.get_json()
     action = data.get('action')
-    response = requests.post(f'{ESP32_IP_ADDRESS}{action}')
+    try:
+        response = requests.post(f'{ESP32_IP_ADDRESS}{action}')
+        response.raise_for_status()  # Check for errors
+        return response.text
+    except requests.exceptions.RequestException as e:
+        return jsonify({'error': f"Error sending action to ESP32: {e}"}), 500
 
-    return response.text
+def esp32_thread():
+    while True:
+        fetch_esp32_data()
+        time.sleep(1)  # Fetch every second
 
 if __name__ == '__main__':
+    # Start a background thread to fetch ESP32 data periodically
+    threading.Thread(target=esp32_thread).start()
 
     app.run(debug=True)
