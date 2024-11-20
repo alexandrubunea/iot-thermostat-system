faf22cfc307d953949b768ecc476e8f5b86167fe
Bunea Alexandru <alexandrubunea03@gmail.com>
2024-11-20 21:35:17 +0200

    Update Progress made for the connection between the web server and ESP32.

--- Web/App.py
+++ Web/App.py
@@ -1,30 +1,50 @@
+import requests
 from flask import Flask, render_template, jsonify, request
+
+ESP32_IP_ADDRESS = 'http://192.168.1.140/api/'
 
 app = Flask(__name__)
 
-# Existing variables
-current_temperature = 58
-current_humidity = 65
-running_time = 125
+current_data = {
+    'currentTemperature': None,
+    'currentHumidity': None,
+    'targetTemperature': None,
+    'runningTime': None
+}
+
+def fetch_esp32_data():
+    global current_data
+
+    response = requests.get(
+        f'{ESP32_IP_ADDRESS}fetch_data',
+        headers={'Content-Type': 'application/json'}
+    )
+    data = response.json()
+
+    current_data = {
+        'temperature': data['currentTemperature'],
+        'humidity': data['currentHumidity'],
+        'target_temperature': data['targetTemperature'],
+        'running_time': data['runningTime']
+    }
 
 @app.route('/')
 def display_temperature():
-    return render_template('index.html')
+    fetch_esp32_data()
+    return render_template('index.html', **current_data)
 
 @app.route('/data')
 def get_data():
-    return jsonify({
-        "temperature": current_temperature,
-        "humidity": current_humidity,
-        "running_time": running_time
-    })
+    return jsonify(current_data)
 
 @app.route('/button-press', methods=['POST'])
 def handle_button_press():
     data = request.get_json()
     action = data.get('action')
-    print(f"Button pressed: {action}")
-    return jsonify({"status": "success", "action_received": action})
+    response = requests.post(f'{ESP32_IP_ADDRESS}{action}')
+
+    return response.text
 
 if __name__ == '__main__':
+
     app.run(debug=True)
