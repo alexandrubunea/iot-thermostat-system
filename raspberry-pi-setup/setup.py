0ca2b81d750b4d4cd7111ad3bf679b5147836bde
Bunea Alexandru <alexandrubunea03@gmail.com>
2024-11-21 15:01:43 +0200

    setup.py is now complete, removed the static allocation of the esp32 because of some issues, the ip of the esp32 will be automatically found when the web application will start

--- raspberry-pi-setup/setup.py
+++ raspberry-pi-setup/setup.py
@@ -5,18 +5,13 @@
     will be able to be configured from the web application and connect to any network, just by
     connecting to the Pi's hotspot and accessing the web application.
 
-    The ESP32 should be turned on and configured to connect to the Raspberry Pi's hotspot, because
-    the script will not finish running until it gets ESP32's MAC address so it can be bound to
-    a permanent ip address.
-
     This script is tested only with the Raspberry Pi OS. I don't know how compatible is with any
     other Debian distribution.
 
     Usage: sudo python setup.py
 """
 
-import subprocess, re
-import time
+import subprocess
 from datetime import datetime
 
 REQUIRED_PACKAGES = [
@@ -70,8 +65,6 @@ WantedBy=multi-user.target hostapd.service
 
 LOG_FILE = 'log.txt'
 
-ESP32_IP_ADDRESS = '192.168.50.100'
-
 def run_command(command):
     with open(LOG_FILE, 'a') as log:
         result = subprocess.run(command,
@@ -123,50 +116,8 @@ def configure_network():
     run_command('sudo systemctl restart dnsmasq')
     run_command('sudo systemctl restart hostapd')
 
-def search_for_esp32():
-    print("Searching for ESP32...")
-
-    esp32_mac = None
-    tries = 0
-
-    while not esp32_mac:
-        out, _ = run_command('sudo arp | grep "esp32"')
-
-        lines = out.splitlines()
-        for line in lines:
-            # Match lines containing MAC addresses
-            match = re.search(r'([0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2})', line,
-                              re.IGNORECASE)
-            if not match:
-                continue
-
-            mac = match.group(1).upper()
-            esp32_mac = mac
-
-            break
-
-        if not esp32_mac:
-            print('No ESP32 device found! Trying again in 3 seconds...')
-
-            # Troubleshooting
-            tries += 1
-            if tries % 3 == 0:
-                run_command('sudo systemctl restart hostapd')
-
-            time.sleep(3)
-
-    # Update dnsmasq configuration for static IP
-    dnsmasq_entry = f'dhcp-host={esp32_mac},{ESP32_IP_ADDRESS}\n'
-    write_to_file('/etc/dnsmasq.d/esp32_static.conf', dnsmasq_entry)
-
-    # Restart dnsmasq to apply changes
-    run_command('sudo systemctl restart dnsmasq')
-
-    print(f'ESP32 device with MAC {esp32_mac} assigned to IP {ESP32_IP_ADDRESS}')
-
 if __name__ == '__main__':
     check_for_dependencies()
     configure_network()
-    search_for_esp32()
 
-    print("Setup complete! Please restart the Raspberry Pi & ESP32.")
+    print("SETUP COMPLETE")
